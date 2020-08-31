4adffd196ce4e32f9777d2e11dbbba303a506877
hkj <haokangjiang@qq.com>
2020-08-31 15:54:47 +0800

    add MD5 for password

---
+++ Atcrowdfunding-common/src/main/java/com/atguigu/atcrowdfunding/util/AjaxResult.java
@@ -0,0 +1,22 @@
+package com.atguigu.atcrowdfunding.util;
+
+public class AjaxResult {
+    private Boolean success;
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+}
